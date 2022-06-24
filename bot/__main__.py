7749150ba2be8a763ab5ec260f47d466a40dcac6
p0lygun <solankivibhakar82@gmail.com>
2022-06-25 02:15:53 +0530

    add a check to check if we are testing DB

--- bot/__main__.py
+++ bot/__main__.py
@@ -10,21 +10,23 @@ try:
         from .pgsql import ConnectionWrapper
         con = ConnectionWrapper()
         con.ensure_base_tables()
-
+        if os.getenv('DB_DEBUG', None):
+            logger.debug(con.exists(('channel_id', 989976538067918968), table='vc_data'))
     except psycopg2.OperationalError:
         raise
 
     try:
-        from bot.bot import PVCBot
+        if not os.getenv('DB_DEBUG', None):
+            from bot.bot import PVCBot
 
-        bot_ = PVCBot(debug_guilds=[
-            829265067073339403,  # gala
-        ], con_=con)
-        token = os.getenv("PVC_TOKEN", None)
-        if token is None:
-            raise ValueError("TOKEN not found, check env file")
-        bot_.load_custom_cogs()
-        bot_.run(token)
+            bot_ = PVCBot(debug_guilds=[
+                829265067073339403,  # gala
+            ], con_=con)
+            token = os.getenv("PVC_TOKEN", None)
+            if token is None:
+                raise ValueError("TOKEN not found, check env file")
+            bot_.load_custom_cogs()
+            bot_.run(token)
     except ConnectionError as e:
         logger.critical(f"Unable to connect to Discord. exit error {e}")
         raise
